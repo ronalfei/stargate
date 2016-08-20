a6993febc37471d4ad3fb675844060aab260785f
wangfei19 <wangfei19@baidu.com>
2016-08-20 17:35:34 +0800

    Rebirth

--- start-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-erl -boot start_sasl -sname stargate@localhost -pa ebin -pa deps/*/ebin -s stargate -s reloader +K true -setcookie sg \
-    -eval "io:format(\"* Eventsource: http://localhost:8080/eventsource~n~n\")."
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-NAME="stargate"
-screen -dmS $NAME
-screen -S $NAME -X screen erl -sname stargate@localhost -pa ebin -pa deps/*/ebin -s stargate -s reloader +K true -setcookie stargate\
-    -eval "io:format(\"* Eventsource: http://localhost:8080/eventsource~n~n\"). "
