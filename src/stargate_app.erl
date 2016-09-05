83f9ddc04181a9fa97c48af056d7de3a92b33877
wangfei19 <wangfei19@baidu.com>
2016-09-05 19:09:31 +0800

    add ensure_application_start

--- src/stargate_app.erl
+++ src/stargate_app.erl
@@ -9,10 +9,9 @@ start() ->
 
 start(_Type, _Args) ->
 	ok = lager:start(),
-    application:start(sasl),
-    application:start(crypto),
-    application:start(ranch),
-    application:start(poolboy),
+    ok = ensure_application_start(sasl),
+    ok = ensure_application_start(crypto),
+    ok = ensure_application_start(ranch),
 	stargate_sup:start_link().
 
 stop(_State) ->
@@ -21,3 +20,11 @@ stop(_State) ->
 
 log(Level) ->
 	lager:set_loglevel(lager_console_backend, Level).
+
+
+ensure_application_start(App) ->
+    case application:start(App) of 
+        ok -> ok;
+        {error,{already_started,sasl}} -> ok;
+        _Any -> io:format("application ~p start error: ~p", [App, _Any]), error
+    end.
